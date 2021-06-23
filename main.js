b5cad05a309f3f9d3664531a7ef6d532cdd1b654
admin <michael.slyusar@gmail.com>
2021-06-23 20:20:58 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function isValid(element){
+    
+    if(!element.value || element.value.length <= 3){
+        element.style.backgroundColor = "red";
+    }else{
+        element.style.backgroundColor = "white";
+    }
+} 
+
+
+function doSomething(){
+
+}
